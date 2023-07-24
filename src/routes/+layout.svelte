8b3195fb8b5cd6748d9ab1e90c84b05fd4ffc80d
Yannic Brügger <yannic.bruegger@gmail.com>
2023-07-24 19:19:34 +0200

    add prerendering

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import Header from '../components/header.svelte';
   import Footer from '../components/footer.svelte';
+
+  export const prerender = true;
 </script>
 
 <Header></Header>
