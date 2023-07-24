a02b0d11e1891304fa94bb7285f8f3d349172719
Yannic Brügger <yannic.bruegger@gmail.com>
2023-07-24 19:26:28 +0200

    remove prerender

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import Header from '../components/header.svelte';
   import Footer from '../components/footer.svelte';
-
-  export const prerender = true;
 </script>
 
 <Header></Header>
